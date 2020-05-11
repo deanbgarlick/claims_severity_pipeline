35d6403820b070fe0ac0367f6fef91126b4b9f4f
Dean Garlick <deanbgarlick@gmail.com>
2020-05-11 15:06:37 +0100

    Add function to James Stein enocode X

--- pipeline/encode_features.py
+++ pipeline/encode_features.py
@@ -14,8 +14,7 @@ def tukey_outlier_test(pd_series):
     return outliers
 
 
-def normalize_y():
-    y_train = read_csv("data/y_train.csv", index_col="id").squeeze()
+def fit_transform_normalize_y(y_train):
     y_train_bx, _ = boxcox(y_train)
     outliers = tukey_outlier_test(y_train_bx)
     y_train_outliers_removed = y_train.drop(outliers)
@@ -24,5 +23,18 @@ def normalize_y():
     return y_train_bx_whitened, bx_lambda
 
 
+def tranform_normalize_y(y, bx_lambda):
+    y_bx, bx_lambda = boxcox(y, bx_lambda)
+    y_bx_whitened = (y_bx - y_bx.mean()) / y.std()
+    return y_bx_whitened
+
+
 def fit_X_encoder():
-    X_train = read_csv()
+    X_train = read_csv("data/X_train.csv", index_col="id")
+    X_cat_features = X_train.dtypes[X_train.dtypes == 'object'].index
+    y_train = read_csv("data/y_train.csv", index_col="id").squeeze()
+    y_train_normalized, bx_lambda = fit_transform_normalize_y(y_train)
+    js_encoder = JamesSteinEncoder(cols=list(X_cat_features), randomized=True, sigma=0.0001)
+    X_train_encoded = js_encoder.fit_transform(X_train, y_train_normalized)
+    X_train_encoded.to_csv('X_train_encoded.csv')
+
