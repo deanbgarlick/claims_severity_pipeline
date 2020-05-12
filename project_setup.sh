fd2e532db2955a5f59db0136e26741a3d1193379
Dean Garlick <51761439+deanbgarlick@users.noreply.github.com>
2020-05-12 02:30:13 +0100

    Update project_setup.sh

--- project_setup.sh
+++ project_setup.sh
@@ -3,6 +3,6 @@ mkdir artifacts
 mkdir data
 unzip allstate-claims-severity.zip -d data
 rm allstate-claims-severity.zip
-conda env create -f environment.yml
+conda env create -f environment.yml --prefix envs python=3.6
 conda activate claims_severity_pipeline_env
 python main.py
