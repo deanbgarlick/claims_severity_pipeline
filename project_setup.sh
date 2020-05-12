2c8babe8fdfce6a3315a624904136ee0730364d4
Dean Garlick <deanbgarlick@gmail.com>
2020-05-12 11:57:28 +0100

    Finish command line interface

--- project_setup.sh
+++ project_setup.sh
@@ -5,4 +5,4 @@ unzip allstate-claims-severity.zip -d data
 rm allstate-claims-severity.zip
 conda env create -f environment.yml --prefix envs python=3.6
 conda activate claims_severity_pipeline_env
-python main.py
+mlflow run . -P n_ei=10
